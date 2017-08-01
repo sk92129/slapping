172eba7e6b0f3c3bccb32b5043b74b9acc1d9e54
matthewkang <matthkang@gmail.com>
2017-07-31 20:41:54 -0700

    change msg

--- src/main/java/com/seankang/slapping/main/Main.java
+++ src/main/java/com/seankang/slapping/main/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
 	public static void main (String [] args) {
 		
-		System.out.println("starting slapping game");
+		System.out.println("starting matthew game");
 		BattleData.loadBattleData();
 		View view = new View();
 
